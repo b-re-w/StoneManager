346a86c8b634f95e01a46bdc595fb2071744da08
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-12-12 01:16:32 +0900

    Design: Change Colorset

--- common/src/commonMain/kotlin/io/github/irack/stonemanager/ui/theme/Color.kt
+++ common/src/commonMain/kotlin/io/github/irack/stonemanager/ui/theme/Color.kt
@@ -1,7 +1,6 @@
 package io.github.irack.stonemanager.ui.theme
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -11,8 +10,6 @@ import androidx.compose.ui.graphics.Color
 val colorLight = lightColorScheme(
     primary = Color(0xFFF4F8FE),
     onPrimary = Color(0xFF6E7C96),
-    secondary = Color(0xFF03DAC5),
-    tertiary = Color(0xFF3700B3),
     background = Color(0xFFE9F1FD),
     onBackground = Color(0xFFA2B0C9)
 )
@@ -20,8 +17,6 @@ val colorLight = lightColorScheme(
 val colorDark = darkColorScheme(
     primary = Color(0xFF25262B),
     onPrimary = Color(0xFFFFFFFF),
-    secondary = Color(0xFF03DAC5),
-    tertiary = Color(0xFF3700B3),
     background = Color(0xFF1A1C20),
     onBackground = Color(0xFF767171)
 )
@@ -29,17 +24,28 @@ val colorDark = darkColorScheme(
 abstract class AppColorSet {
     abstract val lightShadow: Color
     abstract val darkShadow : Color
+
+    companion object {
+        val isLight
+            @Composable get() = !isTimeInNightPeriod()
+
+        val currentColorScheme
+            @Composable get() = MaterialTheme.colorScheme
+
+        val currentAppColorSet
+            @Composable get() = if (isLight) LightColorSet else DarkColorSet
+    }
 }
 
 object LightColorSet: AppColorSet() {
-    override val lightShadow = Color(0xF0FFFFFF)
-    override val darkShadow = Color(0x4F2D2670)
+    override val lightShadow = Color(0xD0FFFFFF)
+    override val darkShadow = Color(0x402D2670)
 }
 
 object DarkColorSet: AppColorSet() {
-    override val lightShadow = Color(0x66494949)
-    override val darkShadow = Color(0x66000000)
+    override val lightShadow = Color(0xA6494949)
+    override val darkShadow = Color(0xF0000000)
 }
 
 val appColorSet
-    @Composable get() = if (MaterialTheme.colors.isLight) LightColorSet else DarkColorSet
+    @Composable get() = AppColorSet.currentAppColorSet
