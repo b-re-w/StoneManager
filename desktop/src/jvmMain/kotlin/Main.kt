2a8eabd4746ec2926d5e27bb2f4e63b6b29c0609
IRACK (CHAEUN) <IRACK000@gmail.com>
2023-12-14 20:36:56 +0900

    Error: MacOS Error (No Idea to fix this)

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -6,15 +6,15 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.abhilash.apps.composecolorpicker.ColorPicker
 import io.github.irack.stonemanager.App
-import io.github.irack.stonemanager.resource.localization.localizedString
-import io.github.irack.stonemanager.util.getPlatform
+import io.github.irack.stonemanager.`interface`.resource.localization.localizedString
+import io.github.irack.stonemanager.`interface`.resource.util.getPlatform
 
 
 fun main() = application {
     val platform = getPlatform().name
     val icon = when {
         platform.contains("Windows") -> painterResource("icon.ico")
-        platform.contains("Mac") -> painterResource("icon.icns")
+        //platform.contains("Mac") -> painterResource("icon.icns")
         platform.contains("Linux") -> painterResource("icon.png")
         else -> null
     }
